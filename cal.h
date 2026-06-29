113fe4f40b828887046397b8f258c27906f88854
web-crea <liuyan1625@gmail.com>
2026-06-29 10:56:00 -0700

    Update cal.h 01

--- cal.h
+++ cal.h
@@ -1,12 +1,12 @@
 int add(int a, int b){
-  return a + b;
+    return a + b;
 }
 int minus(int c, int d){
-  return c - d;
+    return c - d;
 }
 int mul(int e, int f){
-  return e * f;
+    return e * f;
 }
 int divide(int g, int h){
-  return g / h;
+    return g / h;
 }
